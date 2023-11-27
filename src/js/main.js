feaace735c10954095be510fd4ea81accb9f10e3
Jorge <jorge.ed.ribeiro00@gmail.com>
2023-11-27 08:59:48 -0300

    Reposicionamento do carrossel

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,7 @@ const dataWebSite = {
      deleniti fugiat nihil facilis, quidem atque quisquam? Unde eveniet odit dolor voluptatum saepe magnam veniam?</p>`
 };
 
-const onLoad = () => {
     textAboutMe.innerHTML = dataWebSite.aboutMe;
-}
 
 // passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const carouselButtons = document.querySelectorAll('.buttonCarousel');
