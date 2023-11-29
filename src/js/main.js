2fbd320d86ae15945be72e3c55fd3359f89c85ce
Jorge <jorge.ed.ribeiro00@gmail.com>
2023-11-29 10:51:27 -0300

    Simplificação do carrossel

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 const textAboutMe = document.querySelector('#aboutMe');
 
+
 const dataWebSite = {
     aboutMe: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsum, voluptatibus cupiditate labore eligendi tenetur
-     deleniti fugiat nihil facilis, quidem atque quisquam? Unde eveniet odit dolor voluptatum saepe magnam veniam?</p>`
+    deleniti fugiat nihil facilis, quidem atque quisquam? Unde eveniet odit dolor voluptatum saepe magnam veniam?</p>`
 };
 
 const onLoad = () => {
@@ -11,37 +12,26 @@ const onLoad = () => {
 
 window.addEventListener("load", onLoad);
 
-// passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const carouselButtons = document.querySelectorAll('.buttonCarousel');
-const images = document.querySelectorAll('.imageCarousel');
+let imagesCarousel = document.getElementById('imageCarousel');
 
-// passo 2 - dar um jeito de identificar o clique do usuário no botão
-carouselButtons.forEach((buttonCarousel, indice) => {
-    buttonCarousel.addEventListener('click', () => {
-        deactivateSelectedButton();
+let index = 100;
 
-        selectCarouselButton(buttonCarousel);
+imagesCarousel.src = `https://picsum.photos/id/${index}/200/200`;
 
-        hideActiveImage();
+carouselButtons.forEach((buttonCarousel, i) => {    
+buttonCarousel.addEventListener('click', () => {
+        const selectedButton = document.querySelector('.selected');
+        selectedButton.classList.remove('selected');
+        
+        let imageIndex = index + i;
+        let activeImg = `https://picsum.photos/id/${imageIndex}/200/200`;
+        imagesCarousel.src = activeImg;
+        
+        buttonCarousel.classList.add('selected');
+    });
+});
 
-        showHiddenImage(indice);
-    })
-})
 
-function deactivateSelectedButton() { // passo 3 - desmarcar o botão selecionado anterior
-    const selectedButton = document.querySelector('.selected');
-    selectedButton.classList.remove('selected');
-}
 
-function selectCarouselButton(buttonCarousel) { // passo 4 - marcar o botão clicado como se estivesse selecionado
-    buttonCarousel.classList.add('selected');
-}
 
-function hideActiveImage() { // passo 5 - esconder a imagem ativa de fundo
-    const activeImage = document.querySelector('.activeImg');
-    activeImage.classList.remove('activeImg');
-}
-
-function showHiddenImage(indice) { // passo 6 - fazer aparecer a imagem correspondente ao botão clicado
-    images[indice].classList.add('activeImg');
-}
