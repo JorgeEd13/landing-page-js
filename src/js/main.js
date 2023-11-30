7d2893ad0b18ba847f5929930238f264f7b720b0
Jorge <jorge.ed.ribeiro00@gmail.com>
2023-11-30 10:26:44 -0300

    Minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -13,21 +13,25 @@ const onLoad = () => {
 window.addEventListener("load", onLoad);
 
 const carouselButtons = document.querySelectorAll('.buttonCarousel');
+const loadingWarning = document.querySelector('loadImg');
 let imagesCarousel = document.getElementById('imageCarousel');
 
 let firstImgId = 100;
 
 imagesCarousel.src = `https://picsum.photos/id/${firstImgId}/500`;
 
-carouselButtons.forEach((buttonCarousel, i) => {    
-buttonCarousel.addEventListener('click', () => {
+carouselButtons.forEach((buttonCarousel, i) => {
+    buttonCarousel.addEventListener('click', () => {
+        // imagesCarousel.style.display = 'none';
+
+
         const selectedButton = document.querySelector('.selected');
         selectedButton.classList.remove('selected');
-        
+
         let imageIndex = firstImgId + i;
         let activeImg = `https://picsum.photos/id/${imageIndex}/500`;
         imagesCarousel.src = activeImg;
-        
+
         buttonCarousel.classList.add('selected');
     });
 });
